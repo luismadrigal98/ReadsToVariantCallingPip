ba20f5a9f6b3ed9589d73f1308de08c50feb9b9d
Luis <madrigalrocalj@gmail.com>
2025-03-23 22:42:32 -0500

    Minor improvements made

--- StampyRunner.py
+++ StampyRunner.py
@@ -184,7 +184,7 @@ def main():
             sys.exit(1)
         
         generate_stampy_jobs(args.input_dirs, args.output_dirs, args.job_dirs,
-                    args.reference, args.stampy_path, python_2_7_path=args.python_2_7_path,
+                    args.reference, args.stampy_path, args.python_2_7_path,
                     partition=args.partition, time=args.time, email=args.email,
                     mem_per_cpu=args.mem_per_cpu, cpus=args.stampy_cpus)
         
